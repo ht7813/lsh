db2d2f436e13cdba58211ade7052b435d91a8564
ht7813 <hutao7813@126.com>
2024-03-03 14:43:37 +0800

    edit lenv.c

--- src/lenv.c
+++ src/lenv.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
         get(argv[2]);
     }else if (argc == 4 && !strcmp(argv[1], "--set")) {
         set(argv[2], argv[3]);
-    }else if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h") || argc == 1)
+    }else if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h"))
     {
         help(0);
     }else{

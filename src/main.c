ae92f54fc1a3ece38f9a1fec16b1b8776f37557b
ht7813 <hutao7813@126.com>
2024-09-13 18:20:59 +0800

    Fix Non Use Std Getline(make CFLAGS=-DLSH_NOT_USE_STD_GETLINE) bug

--- src/main.c
+++ src/main.c
@@ -3,40 +3,40 @@
 char *lsh_read_line(void)
 {
 #ifdef LSH_NOT_USE_STD_GETLINE
-    int bufsize = LSH_RL_BUFSIZE;
-    int position = 0;
-    char *buffer = malloc(sizeof(char) * bufsize);
-    int c;
+int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
 
-    if (!buffer) {
+  if (!buffer) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    // Read a character
+    c = getchar();
+
+    if (c == EOF) {
+      exit(EXIT_SUCCESS);
+    } else if (c == '\n') {
+      buffer[position] = '\0';
+      return buffer;
+    } else {
+      buffer[position] = c;
+    }
+    position++;
+
+    // If we have exceeded the buffer, reallocate.
+    if (position >= bufsize) {
+      bufsize += LSH_RL_BUFSIZE;
+      buffer = realloc(buffer, bufsize);
+      if (!buffer) {
         fprintf(stderr, "lsh: allocation error\n");
         exit(EXIT_FAILURE);
+      }
     }
-
-    while (1)
-    {
-        // Read a character
-        c = getchar();
-
-        // If we hit EOF, replace it with a null character and return.
-        if (c == EOF || c == '\n') {
-            buffer[position] = c;
-            return buffer;
-        } else {
-            buffer[position] = c;
-        }
-        position++;
-
-        // If we have exceeded the buffwe, reallocate.
-        if (position >= bufsize) {
-            bufsize += LSH_RL_BUFSIZE;
-            buffer = realloc(buffer, bufsize);
-            if (!buffer) {
-                fprintf(stderr, "lsh: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
+  }
 #else
     char *line = NULL;
     ssize_t bufsize = 0; // have getline allocate a buffer for us
